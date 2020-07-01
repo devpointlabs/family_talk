1e5e7c56408ea2f62ce7a9d2581b05e67e298e83
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-07-01 17:31:33 -0600

    user update working, added cloudinary and dropzone

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -39,6 +39,14 @@ export class AuthProvider extends React.Component {
         console.log(res);
       })
   }
+
+  updateUser = (id, user) => {
+    let data = new FormData();
+    data.append('file', user.file);
+    debugger
+    axios.put(`/api/users/${id}?name=${user.name}&email=${user.email}`, data)
+      .then( res => this.setState({ user: res.data, }) )
+  }
   
   render() {
     return (
@@ -49,6 +57,7 @@ export class AuthProvider extends React.Component {
         handleLogin: this.handleLogin,
         handleLogout: this.handleLogout,
         setUser: (user) => this.setState({ user, }),
+        updateUser: this.updateUser,
       }}>
         { this.props.children }
       </AuthContext.Provider>
