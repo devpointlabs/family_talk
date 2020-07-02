98b24b8ea7b93de5efe365453b307df2d7232af3
Kyle Dunshee <kpdunshee@gmail.com>
2020-07-02 17:18:59 -0600

    extra fields added, landing page started

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,8 +9,12 @@ class Api::UsersController < ApplicationController
     user = User.find(params[:id])
     user.name = params[:name] ? params[:name] : user.name
     user.email = params[:email] ? params[:email] : user.email
+    user.first_name = params[:first_name] ? params[:first_name] : user.first_name
+    user.last_name = params[:last_name] ? params[:last_name] : user.last_name
     file = params[:file]
-    if file != "undefined"
+    # binding.pry
+    if file != "undefined" && file != ""
+    
       begin
       #cloudinary stuff here
         cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true, resource_type: :auto)
@@ -21,6 +25,7 @@ class Api::UsersController < ApplicationController
         end
     end
     if user.save
+    
       render json: user
     else
       render json: { errors: user.errors.full_messages }, status: 422
@@ -28,6 +33,6 @@ class Api::UsersController < ApplicationController
   end
   private
     def user_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:email, :password, :first_name, :last_name, :name)
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   
   protected
   def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name])
       end
 
 end
