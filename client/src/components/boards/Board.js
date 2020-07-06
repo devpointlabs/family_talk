e868471640274044d5865f332602a5e35ab5769d
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-07-06 16:57:00 -0600

    minor change

--- client/src/components/boards/Board.js
+++ client/src/components/boards/Board.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import BoardForm from "./BoardForm"
 
 export default function Board(props) {
   const [board, setBoard] = useState({})
