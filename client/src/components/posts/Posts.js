158b208fb4c42eb9364df1bdc53b95fc28a22465
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-07 20:46:36 -0600

    render function is working and data is displaying

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import Axios from "axios"
+import { Image, Card, Icon  } from "semantic-ui-react"
 
 const Posts = ({boardId}) => {
   const [posts, setPosts] = useState([])
@@ -9,11 +10,22 @@ const Posts = ({boardId}) => {
       .then(res => {
         setPosts(res.data)
       })
-  },[])
+  }, [])
+  
+  function renderPosts() {
+    return posts.map(post => (
+      <Card key={post.id}>
+        <h3> {post.title}</h3>
+        <p>{post.description}</p>
+        <Image src={post.image}/>
+      </Card>
+    ))
+  }
 
   return (
     <>
       <h2>Posts</h2>
+      {renderPosts()}
     </>
   )
 }
