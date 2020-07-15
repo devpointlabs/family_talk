66b0cd219cc50371c34c63a4ae2ce00e4f73bc30
Shawn Smith <srsmith11@live.com>
2020-07-15 14:17:14 -0600

    trying to work locally

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,5 @@
 class Board < ApplicationRecord
   has_many :users, through: :user_boards
   has_many :user_boards, dependent: :destroy
-  
   has_many :posts, dependent: :destroy
 end
---
+++ client/src/providers/BoardProvider.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+
+
+const BoardContext = React.createContext();
+export const BoardConsumer = BoardContext.Consumer;
+
+export class BoardProvider extends React.Component {
+  state = { board: null, };
+
+  getBoard = (boardId) => {
+    axios.get(`api/boards/${boardId}`)
+      .then(res => {
+        this.setState({board: res.data, })
+      })
+      .catch(res => {
+        console.log("error")
+      })
+  }
+
+  render() {
+    return (
+      <BoardContext.Provider value = {{
+        ...this.state,
+        getBoard: this.getBoard,
+      }}>
+        {this.props.children}
+      </BoardContext.Provider>
+    )
+  }
+}
