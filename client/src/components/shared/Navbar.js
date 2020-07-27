a6a8674a68b544fd1b7202ac84c01361100df70c
Kyle Dunshee <kpdunshee@gmail.com>
2020-07-27 15:53:31 -0600

    added logo to navbar

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { AuthConsumer, } from "../../providers/AuthProvider";
-import { Menu, Dropdown, } from 'semantic-ui-react'
+import { Menu, Dropdown, Image} from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
+import MSLogo from '../../assets/mediumsmall.jpg'
+import './Navbar.css'
 
 class Navbar extends React.Component {
+
+
+   trigger = (
+  <span>
+    <Image src={MSLogo} className='logo'/>
+  </span>
+)
   
   rightNavItems = () => {
     const { auth: { user, handleLogout, }, location, } = this.props;
     
     if (user) {
       return (
-          <Dropdown text = 'User' icon = 'user'>
+          <Dropdown text = 'User' icon = 'user' >
         <Dropdown.Menu>
             <Dropdown.Item as ={Link} to= '/settings'>Settings</Dropdown.Item>
             <Dropdown.Item onClick = {() => handleLogout(this.props.history)}>Logout</Dropdown.Item>
@@ -31,16 +40,19 @@ class Navbar extends React.Component {
 
   render() {
     return( 
-      <div>
-  <Dropdown>
+      <div className="navbar">
+  <Dropdown  trigger={this.trigger} icon={null}>
     <Dropdown.Menu>
-      <Dropdown.Item as={Link} to='/'>Home</Dropdown.Item>
+      <Dropdown.Item as={Link} to='/'>Home </Dropdown.Item>
       <Dropdown.Item as ={Link} to='/landingPage'>Family Room</Dropdown.Item>
       <Dropdown.Item as={Link} to='/publicPage'>Explore</Dropdown.Item>
       <Dropdown.Item as={Link} to='/contactUs'>Contact</Dropdown.Item>
     </Dropdown.Menu>
-  </Dropdown>
+        </Dropdown>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center',}}>
   {this.rightNavItems()}
+</div>
+  
   </div>
   )} 
 }
